ff552de4db4e7fefa7f0f1011d296c87d10dc872
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-08-05 10:15:55 +1000

    Correcting validated_data.GET in update methods in serializers

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -20,22 +20,20 @@ class ProjectDetailSerializer(ProjectSerializer):
     instance.title = validated_data.get('title', instance.title)
     instance.description = validated_data.get('description', instance.description)
     instance.goal = validated_data.get('goal', instance.goal)
-    instance.image = validated_data('image', instance.image)
-    instance.is_open = validated_data('is_open', instance.is_open)
-    instance.date_created = validated_data('date_created', instance.date_created)
-    instance.owner = validated_data('owner', instance.owner)
+    instance.image = validated_data.get('image', instance.image)
+    instance.is_open = validated_data.get('is_open', instance.is_open)
+    instance.date_created = validated_data.get('date_created', instance.date_created)
+    instance.owner = validated_data.get('owner', instance.owner)
     instance.save()
     return instance
   
 class PledgeDetailSerializer(PledgeSerializer):
-  # project = ProjectSerializer(many=False, read_only=True)
-  # project = serializers.ReadOnlyField(label='ID')
-
+  
   def update(self,instance,validated_data):
     instance.amount = validated_data.get('amount', instance.amount)
     instance.comment = validated_data.get('comment', instance.comment)
     instance.anonymous = validated_data.get('anonymous', instance.anonymous)
-    instance.project = validated_data('project'(id),instance.project)
-    instance.supporter = validated_data('supporter', instance.supporter)
+    instance.project = validated_data.get('project',instance.project)
+    instance.supporter = validated_data.get('supporter', instance.supporter)
     instance.save()
     return instance
