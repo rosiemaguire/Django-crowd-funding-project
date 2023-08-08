17345f6f5320a1265a3a56ba323db4dcdf2b0813
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-08-08 17:25:36 +1000

    Updating delete URL

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('projects/<int:pk>/', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),
     path('pledges/<int:pk>/', views.PledgeDetail.as_view()),
-    path('api/pledges/<int:pk>/', views.PledgeDeleteView.as_view(), name='pledge-delete'),
-    path('api/projects/<int:pk>/', views.ProjectDeleteView.as_view(), name='project-delete'),
+    path('delete/pledges/<int:pk>/', views.PledgeDeleteView.as_view(), name='pledge-delete'),
+    path('delete/projects/<int:pk>/', views.ProjectDeleteView.as_view(), name='project-delete'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
