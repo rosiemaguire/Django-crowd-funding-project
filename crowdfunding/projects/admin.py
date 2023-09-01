885541c110d60db33d6c3bc01f45b07f602f6d81
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-09-01 18:39:48 +1000

    Issue 3: Allowing Admin view to see soft deleted projects and pledges

--- crowdfunding/projects/admin.py
+++ crowdfunding/projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from projects.models import Project,Pledge
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Pledge)
+class PledgeAdmin(admin.ModelAdmin):
+  pass
