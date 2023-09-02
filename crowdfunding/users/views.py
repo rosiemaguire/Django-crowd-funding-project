ca93ddcdcc98dd9fd2ba7caf7b89e53be5dc1c88
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-09-02 20:37:32 +1000

    Adding restrictions on properties returned in user post requests for non-admin users. Had to update functionality of get user list request as a result

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -7,49 +7,45 @@ from .serializers import CustomUserSerializer
 from .permissions import UserUpdatePermission
 
 class CustomUserList(APIView):
-    # def get_queryset(self, request):
-    #     if request.user.is_staff:
-    #         return CustomUser.objects.all()
-    #     else:
-    #         users = CustomUser.objects.all()
-    #         users = users.values('username','first_name','last_name','email','date_joined')
-    #         users = users.filter(pk=self.request.user.id)
-    #         return users
-    
-    # def get(self,request):
-    #     users = self.get_queryset(request)
-    #     serializer = CustomUserSerializer(users, many=True)
-    #     return Response(serializer.data)
     
     def get(self,request):
         if request.user.is_staff:
             users = CustomUser.objects.all()
             serializer = CustomUserSerializer(users, many=True)
             return Response(serializer.data)
-        else:
-            users = CustomUser.objects.all()
-            
-            # users = users.values('username','first_name','last_name','email','date_joined','last_login')
-            users = users.get(pk=self.request.user.id)
+        elif request.user.is_authenticated:
+            users = CustomUser.objects.all().get(pk=self.request.user.id)
             data = CustomUserSerializer.get_restricted_data(users)
             serializer = CustomUserSerializer(users, data=data,many=True)
             return Response(serializer.initial_data)
+        else:
+            return Response(
+                { "detail": "You do not have permission to perform this action." }, 
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+            
     
     def post(self,request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
             if request.user.is_staff:
                 serializer.save()
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                )
             else:
                 serializer.save(
                     is_staff=False,
                     is_superuser=False
                 )
-                serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+                user = CustomUser.objects.all().get(pk=serializer.data['id'])
+                data = CustomUserSerializer.get_restricted_data(user)
+                results_returned = CustomUserSerializer(user,data=data)
+                return Response(
+                    results_returned.initial_data,
+                    status=status.HTTP_201_CREATED
+                )
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
@@ -58,11 +54,6 @@ class CustomUserList(APIView):
 class CustomUserDetail(APIView):
     permission_classes = [UserUpdatePermission]
 
-    # def get_restricted_data(self,instance):
-    #     return {
-    #         'username': instance.username,
-
-    #     }
     def get_object(self,pk):
         try:
             user = CustomUser.objects.get(pk=pk)
@@ -81,10 +72,6 @@ class CustomUserDetail(APIView):
             data = CustomUserSerializer.get_restricted_data(user)
             serializer = CustomUserSerializer(user,data=data)
             return Response(serializer.initial_data)
-        # if request.user.is_staff:
-        #     return Response(serializer.data)
-        # else:
-        
     
     def put(self,request,pk):
         user = self.get_object(pk)      
