2f9d4e258255ca53f4940d44c8864e9480ebb43c
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-07 20:52:43 +1000

    Did nto realise I was accidentally hard deleting is_deleted pledges when attempting to filter out

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Prefetch
 from .models import Project,Pledge
 from .serializers import ProjectSerializer,PledgeSerializer,ProjectDetailSerializer, PledgeDetailSerializer
 from django.http import Http404
@@ -39,11 +40,10 @@ class ProjectDetail(APIView):
 
     def get_object(self,pk):
         try:
-            projects = Project.objects.all().filter(is_deleted=False)
+            queryset = Pledge.objects.all().filter(is_deleted=False)
+            projects = Project.objects.all().filter(is_deleted=False).prefetch_related(Prefetch('pledges',queryset=queryset))
             project = projects.get(pk=pk)
-            # project = Project.objects.get(pk=pk)
             self.check_object_permissions(self.request,project)
-            project.pledges.all().filter(is_deleted=True).delete()
             return project
         except Project.DoesNotExist:
             raise Http404
